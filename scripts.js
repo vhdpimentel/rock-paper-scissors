405afa3991815c467aa143f6f8162d611ab3828f
Victor Hugo Damo Pimentel <victor_hugo.d.pimentel@hotmail.com>
2022-04-08 21:46:17 -0300

    Add basic files and initial planning

---
+++ scripts.js
@@ -0,0 +1,13 @@
+/*
+ - The user will enter one of these options: 
+   rock, paper or scissors
+ - the browser will come up with its own option
+ - both will be compared
+ - the victorious party will be declared for this round
+ - rounds will go on for a best of five
+ - final victory will be declared
+ - the user will ble able to choose to play again
+
+ - all of the inputs and outputs will happen through the
+   browser console, initially
+*/
